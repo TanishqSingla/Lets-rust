a9b3f475a41a0f51770083cb7b3efe7f589c52dc
TanishqSingla <tanishqsingla00@gmail.com>
2020-09-30 18:09:25 +0530

    Comparing values

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -11,10 +12,17 @@ fn main() {
     println!("Please input your guess");
 
     let mut guess = String::new();
-
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too large"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }

21ca99fe65a8d56a7146ef34254a6e9c9eaec107
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-28 01:03:33 +0530

    added problem structure

--- Exercise/Exercise-01.rs
+++ Exercise/Exercise-01.rs
@@ -1,3 +1,15 @@
 fn main() {
   let s = String::from("book");
+
+  println!(
+    "I have one {}, you ahve two {}",
+    s,
+    you_add_something_here
+  );
+
+}
+
+//Add appropriate parameters, return values, and implementation to this function
+fn pluralize(singular: String) -> String {
+  singular + "s";
 }

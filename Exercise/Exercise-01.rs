ef16ee14946dc67f64b0022a8e7a37fa8009de3d
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-28 01:11:58 +0530

    Added Solution

--- Exercise/Exercise-01.rs
+++ Exercise/Exercise-01.rs
@@ -1,15 +1,18 @@
 fn main() {
   let s = String::from("book");
 
-  println!(
-    "I have one {}, you ahve two {}",
-    s,
-    you_add_something_here
-  );
+  // making a clone and storing it after pluralizing it
+  let pl = pluralize(s.clone());
 
+  println!(
+    "I have one {}, you have two {}",
+    s,
+    // you_add_something_here
+    pl
+  );
 }
 
 //Add appropriate parameters, return values, and implementation to this function
 fn pluralize(singular: String) -> String {
-  singular + "s";
+  singular + "s"
 }
